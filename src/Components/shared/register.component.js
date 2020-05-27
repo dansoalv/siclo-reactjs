da9957db6a88c22399acbbe655d45fa28ed70d09
Daniel Sosa <daniel.soalvirde@gmail.com>
2020-05-26 19:33:42 -0500

    Galeria y formulario

--- src/Components/shared/register.component.js
+++ src/Components/shared/register.component.js
@@ -1,37 +1,47 @@
-import React from "react";
+import React, {Component} from 'react';
 import '../../CSS/register.css'
 
-const RegisterComponent = () => {
 
-    return (
-        <div className="container">
-            <form>
-                <div>
-                    <h5>Crear tu cuenta</h5>
-                    <div className="form-check form-check-inline">
-                        <input  className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
-                        <label className="form-check-label" htmlFor="inlineCheckbox1">Usar mi cuenta de siclo</label>
-                </div>
-        </div>
-            <div className="contactForm" >
-            <input className="input" type="text" placeholder="Nombre Completo"/>
-            <input className="input" type="text" placeholder="Telefono"/>
 
-            <input className="input" type="text" placeholder="Correo electrónico"/>
-            <small className="form-text text-muted msgError" >Correo electrónico inválido</small>
+class RegisterComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checked : false
+        }
+    }
 
-            <input className="input" type="password" placeholder="Contraseña"/>
-            <input className="input" type="password" placeholder="Repetir contraseña"/>
+    render() {
+        return (
+            <div className="container">
+                <form>
+                    <div>
+                        <h5>Crear tu cuenta</h5>
+                        <div className="form-check form-check-inline">
+                            <input  className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" checked={this.state.checked} onChange={() => this.setState({checked: !this.state.checked})}/>
+                            <label className="form-check-label" htmlFor="inlineCheckbox1">Usar mi cuenta de siclo</label>
+                        </div>
+                    </div>
+                    <div className="contactForm" hidden={this.state.checked}>
+                        <input className="input" type="text" placeholder="Nombre Completo"/>
+                        <input className="input" type="text" placeholder="Telefono"/>
+
+                        <input className="input" type="text" placeholder="Correo electrónico"/>
+
+                        <input className="input" type="password" placeholder="Contraseña"/>
+                        <input className="input" type="password" placeholder="Repetir contraseña"/>
+                    </div>
+
+                    <div className="contactForm" hidden={!this.state.checked}>
+                        <input className="input" type="text" placeholder="Correo electrónico"/>
+                        <input className="input" type="password" placeholder="Contraseña"/>
+                    </div>
+                </form>
             </div>
 
-            <div className="contactForm" >
-            <input className="input" type="text" placeholder="Correo electrónico"/>
-            <input className="input" type="password" placeholder="Contraseña"/>
-        </div>
-        </form>
-        </div>
-
-    );
+        );
+    }
 }
 
-export default RegisterComponent
+export default RegisterComponent;
+
